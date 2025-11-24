1a50d10903fca6e03d029353a206e1cbe4959fee
sashasutton <sasha.sutton@etu.univ-amu.fr>
2025-11-24 01:48:35 +0100

    connectDB function finished , problem solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
+import {connectDB} from './lib/db.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes)
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
+    connectDB();
 });
