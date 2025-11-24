6fb65d043a6c994db2f8ee5deb8eec81766cbb06
sashasutton <sasha.sutton@etu.univ-amu.fr>
2025-11-24 17:08:39 +0100

    changed the string to an S

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     email: {
-        type: string,
+        type: String,
         reuired: true,
     },
     username: {
