980ecaf4367c2e69ef9ed2611b409f9bf5bff507
sashasutton <sasha.sutton@etu.univ-amu.fr>
2025-11-24 01:58:06 +0100

    user schema done

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,25 @@
-import {MongoClient} from "mongodb";
-import * as mongoose from "node/inspector.generated.d.ts";
+import mongoose from "mongoose";
 
-const userSchema = new MongoClient()
+const userSchema = new mongoose.Schema({
+    email: {
+        type: string,
+        reuired: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    profilePic:{
+        type: String,
+        default: "",
+    },
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
